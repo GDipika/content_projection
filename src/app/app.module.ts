fd75d920113c757e73acc5a1f0e815d36d44faea
GDipika <guddetidipika11@gmail.com>
2023-07-27 15:28:58 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LanguagesComponent } from './languages/languages.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LanguagesComponent
   ],
   imports: [
     BrowserModule
